82c569b9b72ea9f0f090e8963ff17e92d4318ea9
MikeAyrton <82711970+MikeAyrton@users.noreply.github.com>
2022-03-16 18:35:47 +0500

    m122e5

--- finallproject/finallproject/settings.py
+++ finallproject/finallproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)cyrzksme5p!)(gn8m(6u7gtnf=m^)_(eoz)yzc_&lo6y@ys(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'economerceh.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
